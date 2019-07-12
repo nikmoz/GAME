5799a61c415512329e712b0eb395eecf784d7baf
Nikita <mukuta95@gmail.com>
2019-07-12 18:12:24 +0300

    Templated State Machine is ready(don't sure if I still want to use it)

--- TagXmlParser.h
+++ TagXmlParser.h
@@ -6,20 +6,23 @@
 
 namespace TagXmlParser
 {
+	const std::regex TagRegex("<[^>]+>|\\n>");
 	template <typename TagContent>
 	TagContent FindTag(std::ifstream& File, const std::string& TagName)
 	{
+		const auto Tag="<"+TagName+">";
+
 		File.clear();
 		File.seekg(0, std::ifstream::beg);
 
 		std::smatch Matches;
-		const std::regex Tag("<"+TagName+">");//NOTE(Nick):Check if regex is expensive to create
 
 		std::string FileLine;
 		while (getline(File, FileLine))
 		{
-			std::regex_search(FileLine, Matches,Tag);
-			if (!Matches.empty()) {
+			std::regex_search(FileLine, Matches,TagRegex);
+			if (!Matches.empty()&& Matches[0]==Tag)
+			{
 				TagContent Tmp;
 				File>>Tmp;
 
@@ -33,18 +36,20 @@ namespace TagXmlParser
 	template <typename TagContent>
 	std::vector<TagContent> FindAllTags(std::ifstream& File, const std::string& TagName)
 	{
+		const auto Tag="<"+TagName+">";
+
 		File.clear();
 		File.seekg(0, File.beg);
 
 		std::smatch Matches;
-		const std::regex Tag("<"+TagName+">");//NOTE(Nick):Check if regex is expensive to create
 		std::vector<TagContent> FoundContent={};
 
 		std::string  FileLine;
 		while (getline(File, FileLine))
 		{
-			std::regex_search(FileLine, Matches,Tag);
-			if (!Matches.empty()) {
+			std::regex_search(FileLine, Matches,TagRegex);
+			if (!Matches.empty()&& Matches[0]==Tag)
+			{
 				TagContent Tmp;
 				File>>Tmp;
 				FoundContent.push_back(Tmp);
